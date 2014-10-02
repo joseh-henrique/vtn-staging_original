0e0612184712d3c345372da867b6427514d3ddf0
DangNH <dangnh@elarion.com>
2014-10-02 08:45:24 +0700

    add secret key base

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,5 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 PurexNew::Application.config.secret_token = '0282824e1b163843ee394f7e4aa6f1d5d09e56d441ee50d519d10ba54816be5d2fce64f29342809ec3b3662f3130eb003645fb2f01d07e554ef8d005fe3161b2'
+secret = Rails.env.production? ? ENV['SECRET_TOKEN'] : "top_secret_token"
+PurexNew::Application.config.secret_key_base = secret
